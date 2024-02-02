d0b04306d7b0748771082ff564872036044037fe
Kuan Jiun Ying <jiunying2000kjy@gmail.com>
2024-02-02 16:14:35 +0800

    first commit

---
+++ secret.php
@@ -0,0 +1,8 @@
+<?php
+$apiKey= "AIzaSyBXtmwbDNWIMVyXnoGBkjrM9gggqPu3ySs";
+$authDomain= "ems-e-merchant-system.firebaseapp.com";
+    $projectId= "ems-e-merchant-system";
+    $storageBucket= "ems-e-merchant-system.appspot.com";
+    $messagingSenderId= "895039847507";
+    $appId= "1:895039847507:web:d287b9b86346beddafdaa6";
+    $measurementId= "G-5ZWXT5C8S1"; ?>
